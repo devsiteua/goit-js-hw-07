6539a0a9a4f6f4f44dfbe85b50580ab1b6f5d4f9
devsiteua <107812007+devsiteua@users.noreply.github.com>
2026-09-07 00:39:48 +0100

    Complete task 2

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,17 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = images
+  .map(
+    image => `
+      <li class="gallery-item">
+        <img class="gallery-image" src="${image.url}" alt="${image.alt}" />
+      </li>
+    `
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
