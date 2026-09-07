71fc6cc1083b82bc8e7bc185d8d9de586b81def5
devsiteua <107812007+devsiteua@users.noreply.github.com>
2026-09-07 01:16:15 +0100

    Complete task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", () => {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  colorValue.textContent = color;
+});
