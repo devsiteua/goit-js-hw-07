1918ebc4bb2e071e48ae71188c81c8b62738eee3
devsiteua <107812007+devsiteua@users.noreply.github.com>
2026-09-07 01:07:42 +0100

    Complete task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value.trim();
+  const password = event.currentTarget.elements.password.value.trim();
+
+  if (!email || !password) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+});
 
